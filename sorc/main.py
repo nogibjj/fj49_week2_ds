f1b2b9ab48be84c53f570ae4e127ddf6d72754b7
Faraz Jawed <farazjawed@Farazs-Air.lan>
2023-09-10 18:42:34 -0400

    final changes

--- sorc/main.py
+++ sorc/main.py
@@ -12,47 +12,47 @@ spotify = reader()
 print(spotify)
 
 
-# # basic stats
-# def mean():
-#     mean_duration = int(spotify["duration_ms"].mean())
-#     return mean_duration
+# basic stats
+def mean():
+    mean_duration = int(spotify["duration_ms"].mean())
+    return mean_duration
 
 
-# def median():
-#     median_duration = int(spotify["duration_ms"].median())
-#     return median_duration
+def median():
+    median_duration = int(spotify["duration_ms"].median())
+    return median_duration
 
 
-# def mode():
-#     mode_duration = int(spotify["duration_ms"].mode())
-#     return mode_duration
+def mode():
+    mode_duration = int(spotify["duration_ms"].mode())
+    return mode_duration
 
 
-# def std():
-#     std_duration = int(spotify["duration_ms"].std())
-#     return std_duration
+def std():
+    std_duration = int(spotify["duration_ms"].std())
+    return std_duration
 
 
-# # making a plot
-# def viz():
-#     value_counts = spotify["artist_name"].value_counts()
-#     top_10_value_counts = value_counts.head(10)
+# making a plot
+def viz():
+    value_counts = spotify["artist_name"].value_counts()
+    top_10_value_counts = value_counts.head(10)
 
-#     plt.figure(figsize=(20, 12))
-#     plt.bar(top_10_value_counts.index, top_10_value_counts.values)
-#     # Add labels and a title to the plot
-#     plt.xlabel("Top Artists")
-#     plt.ylabel("Number of top tracks")
-#     plt.title("Which artists had the most top tracks in the last few years?")
-#     plt.show()
-
-
-# # def sanity_add(x, y):
-# #     return x + y
+    plt.figure(figsize=(20, 12))
+    plt.bar(top_10_value_counts.index, top_10_value_counts.values)
+    # Add labels and a title to the plot
+    plt.xlabel("Top Artists")
+    plt.ylabel("Number of top tracks")
+    plt.title("Which artists had the most top tracks in the last few years?")
+    plt.show()
 
 
-# # if __name__ == "__main__":
-# #     a = 1
-# #     b = 2
+# def sanity_add(x, y):
+#     return x + y
 
-# #     print(sanity_add(a, b))
+
+# if __name__ == "__main__":
+#     a = 1
+#     b = 2
+
+#     print(sanity_add(a, b))
