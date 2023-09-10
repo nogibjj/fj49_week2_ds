c00180dc4020ff9c0d7cb368d558fd0be448d34f
Faraz Jawed <farazjawed@Farazs-Air.lan>
2023-09-10 17:42:45 -0400

    fixed

---
+++ sorc/test_main.py
@@ -0,0 +1,33 @@
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+
+sys.path.append("/workspaces/fj49_week2_ds")
+from sorc.main import mean, median, mode, std, viz
+
+spotify = pd.read_csv("playlist_2010to2022.csv")
+mean = mean()
+median = median()
+mode = mode()
+std = std()
+viz = viz()
+
+
+def test_mean():
+    assert mean == int(spotify["duration_ms"].mean())
+
+
+def test_median():
+    assert median == int(spotify["duration_ms"].median())
+
+
+def test_mode():
+    assert mode == int(spotify["duration_ms"].mode())
+
+
+def test_std():
+    assert sd == int(spotify["duration_ms"].std())
+
+
+def test_viz():
+    assert len(viz) > 0  # asserts that viz is not empty
